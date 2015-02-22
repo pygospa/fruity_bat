fadcb44b5bb80014f57cb208b6dc46b22f7c0ab8
Kannan Thambiah <pygospa@gmail.com>
2015-02-23 00:22:04 +0100

    Add foreground and animation foreground scrolls 3pxl per frame (update is called every frame, i.e. 1/60s)

--- game.rb
+++ game.rb
@@ -3,16 +3,22 @@ require 'gosu'
 class GameWindow < Gosu::Window
   def initialize(*args)
     super
-
+    @scroll_x = 0
     @background = Gosu::Image.new(self, 'images/background.png', false)
+    @foreground = Gosu::Image.new(self, 'images/foreground.png', false)
   end
 
   def button_down(button)
     close if button == Gosu::KbEscape
   end
 
+  def update
+    @scroll_x +=3
+  end
+
   def draw
     @background.draw(0,0,0)
+    @foreground.draw(@scroll_x,0,0)
   end
 end
 
