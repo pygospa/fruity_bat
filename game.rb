6e6dd9c307200863009535cc65d5d74e33026d9e
Kannan Thambiah <pygospa@gmail.com>
2015-02-23 00:15:22 +0100

    Add constructor and draw function and include background to be drawn

--- game.rb
+++ game.rb
@@ -1,8 +1,18 @@
 require 'gosu'
 
 class GameWindow < Gosu::Window
+  def initialize(*args)
+    super
+
+    @background = Gosu::Image.new(self, 'images/background.png', false)
+  end
+
   def button_down(button)
     close if button == Gosu::KbEscape
+  end
+
+  def draw
+    @background.draw(0,0,0)
   end
 end
 
