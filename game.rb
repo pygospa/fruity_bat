02d99ed21bbc3a6e60f1158372a16b8a93874e25
Kannan Thambiah <pygospa@gmail.com>
2015-02-22 23:53:23 +0100

    Inherit form Gosu::Window to being able to override needed methods

--- game.rb
+++ game.rb
@@ -1,4 +1,10 @@
 require 'gosu'
 
-window = Gosu::Window.new(800,600,false)
+class GameWindow < Gosu::Window
+  def button_down(button)
+    close if button == Gosu::KbEscape
+  end
+end
+
+window = GameWindow.new(800,600,false)
 window.show
