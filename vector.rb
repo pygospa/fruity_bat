2578c49b169fd8cc1cbc2efc00f7bde26722c541
Kannan Thambiah <pygospa@gmail.com>
2015-02-23 23:16:58 +0100

    Add additional vector functionality and tests

--- vector.rb
+++ vector.rb
@@ -1,12 +1,28 @@
-Vec = struct.new(:x, :y) do
+Vec = Struct.new(:x, :y) do
   def set!(other)
-    x = other.x
-    y = other.y
+    self.x = other.x
+    self.y = other.y
+    self
   end
 
-  def +=(other)
-    x += other.x
-    y += other.y
+  def +(other)
+    Vec[x + other.x, y + other.y]
+  end
+
+  def -(other)
+    Vec[x - other.x, y - other.y]
+  end
+
+  def *(scalar)
+    Vec[x*scalar, y*scalar]
+  end
+
+  # 5*Vec will yield an error, becuase ruby expects vec*scalar.
+  # Therfore above method will create an error!
+  #     Vec can't be coerced into Fixnum
+  # The coerce function fixes this by switching the positions!
+  def coerce(left)
+    [self,left]
   end
 end
 
